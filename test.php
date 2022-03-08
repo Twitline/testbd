2597171a14b2649634ef9b2affdbaddfe796f8ab
Geoffrey Launay <glaunay@sio.local>
2022-03-08 15:06:02 +0100

    commit

--- test.php
+++ test.php
@@ -3,16 +3,23 @@ include_once 'index.php';
 $dbInstance=db_connect('groups');
 ?>
 <form method=post>
-    <button type="submit" name="bouton">-5</button>
-    <button type="submit" name="bouton">+5</button>
+    <button type="submit" name="c" value="(-5)">-5</button>
+    <button type="submit" name="c" value="5">+5</button>
 </form>
 <?php
+extract($_POST);
 $a=0;
 $b=5;
+$c??=0;
+if(isset($_POST["c"])){
+    $a=$a+$c;
+    $b=$b+$c;
+}
 
-if (isset($_POST["bouton"])){
-    $a=$a-5;
-    $b=$b+5;
+function countTable(\PDO $db,string $tableName){
+    $sql="SELECT COUNT(*) FROM $tableName;"
+    $st=$db->query($sql);
+    return $st->fetchColumn();
 }
 $sql='SELECT firstname, lastname FROM USER limit '.$a.','.$b.';';
 $statement=$dbInstance->query($sql);
